f4bd7823f65b3763123c19417a4374a1d47bcb31
abhay <imabhaysachan@gmail.com>
2025-04-11 15:15:10 +0530

    fix twitter url

--- components/TwitterWidget.jsx
+++ components/TwitterWidget.jsx
@@ -5,7 +5,7 @@ import React from 'react';
 function TwitterWidget() {
   return (
     <div className="twitter-widget-container">
-      <a className="twitter-timeline" href="https://x.com/Yjssofficial?ref_src=twsrc%5Etfw">Tweets by Yjssofficial</a>
+      <a className="twitter-timeline" href="https://twitter.com/Yjssofficial?ref_src=twsrc%5Etfw">Tweets by Yjssofficial</a>
       <script async src="https://platform.twitter.com/widgets.js" charSet="utf-8"></script>
     </div>
   );
