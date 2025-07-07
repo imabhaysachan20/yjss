7df073580605c8ec7d24d2c95a13c17e7e00db48
abhay <imabhaysachan@gmail.com>
2025-07-07 23:09:06 +0530

    fix

--- app/api/verifyPayment/support/route.js
+++ app/api/verifyPayment/support/route.js
@@ -1,7 +1,7 @@
 import { NextResponse } from 'next/server';
 import crypto from 'crypto';
 import { connectDB } from "@/utils/connectToDb"
-import Member from '@/models/Donerr';
+import Member from '@/models/Doner';
 
 export async function POST(request) {
   try {
