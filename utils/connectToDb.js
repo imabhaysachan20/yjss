b4be21fffb0fb452978c392e5265f118eef2246b
abhay <imabhaysachan@gmail.com>
2025-07-23 11:24:54 +0530

    fix

--- utils/connectToDb.js
+++ utils/connectToDb.js
@@ -18,8 +18,7 @@ export async function connectDB() {
   }
 
   if (!cached.promise) {
-    cached.promise = mongoose.connect(MONGODB_URI, {
-    }).then((mongoose) => mongoose);
+    cached.promise = mongoose.connect(MONGODB_URI).then((mongoose) => mongoose);
   }
 
   cached.conn = await cached.promise;
